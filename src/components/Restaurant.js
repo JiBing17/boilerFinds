5091cc1afffde5538757d5d102933f5b57dcdb93
Ji Bing Ni <jibingni17@gmail.com>
2025-02-18 23:12:52 -0500

    added and styled landing pictures + text

--- src/components/Restaurant.js
+++ src/components/Restaurant.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { useLocation, useParams } from "react-router-dom";
-
+import { cuisineEmojiMapping, cuisineImageMapping } from "../mappings";
+import discoverFood1 from '../pictures/food/discover_food.jpg'
+import discoverFood2 from '../pictures/food/discover_food_2.jpg'
 import Header from './Header'
 const Restaurant = () => {
 
@@ -11,6 +13,55 @@ const Restaurant = () => {
   return (
     <>
         <Header/>
+        <div style={{backgroundColor: "#101010"}}>
+
+          <div className='position-relative w-100' style={{borderBottom: "8px solid #CFB991", boxShadow: "0 0 15px rgba(255, 255, 255, 0.7)" }}>
+            <img src={cuisineImageMapping[restaurant.tags.cuisine]} style={{ marginTop: "3rem", width: "100%", height: "30rem", objectFit: "cover"}}/>
+            <div class="position-absolute top-50 start-50 translate-middle text-white text-center bg-dark bg-opacity-75 p-4 rounded">
+                <h1>A Culinary Journey Awaits</h1>
+                <p>Discover a world of exquisite flavors, inviting ambiance, and unforgettable dining experiences.</p>
+                <button className='btn btn-primary'>View Info</button>
+            </div>
+          </div>
+
+          <div className='d-flex flex-column justify-content-center w-100 text-white gap-4 p-3'>
+
+            <div className='d-flex flex-column-reverse flex-md-row p-4 gap-3'>
+            
+              <div className='d-flex col-12 col-md-6'>
+                  <img className="d-block w-100 rounded" style={{objectFit: "cover", height: "25rem", boxShadow: "0 0 15px rgba(255, 255, 255, 0.7)"}} src={discoverFood1} alt="Food Picture"/>
+              </div>
+
+              <div className='d-flex flex-column align-items-center justify-content-center col-md-6 col-12'>
+                  <h1 className='ms-md-4 ms-0 fw-bold fst-italic text-center'>Discover, Dine, Delight</h1>
+                      <p className='text-center p-3'>
+                        A journey of taste, crafted for those who appreciate great food.
+                        Step into a space where every detail is designed for your enjoyment.
+                      </p>
+              </div>
+              
+            </div>
+            <div className='d-flex flex-column-reverse flex-md-row p-4 gap-3'>
+              <div className='d-flex flex-column align-items-center justify-content-center col-md-6 col-12'>
+                    <h1 className='ms-md-4 ms-0 fw-bold fst-italic text-center'>Your Next Favorite Dining Spot</h1>
+                    <p className='text-center p-3'>
+                      Whether it's a casual bite or a special occasion, great food is always served here.
+                      Indulge in flavors that turn an ordinary meal into something unforgettable.
+                      </p>
+                </div>
+                <div className='d-flex col-12 col-md-6'>
+                    <img className="d-block w-100 rounded" style={{objectFit: "cover", height: "25rem", boxShadow: "0 0 15px rgba(255, 255, 255, 0.7)"}} src={discoverFood2} alt="Food Picture"/>
+                </div>
+
+              
+
+            </div>
+
+          </div>
+        </div>
+        
+
+        
 
     </>
   )
